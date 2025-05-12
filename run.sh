a9d6f3695b477ceb86b5c69f26726c19ed11bbc3
Asif malik <31756470+CodeWithNeo@users.noreply.github.com>
2025-05-12 23:24:28 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run Flask app in background
+python app.py &
+
+# Tunnel using localhost.run (no warning page)
+ssh -R 80:localhost:7860 ssh.localhost.run
